608e4a4a452e2ec9ee28f798128671aa35140bca
Emily-Sam <130898301+Emily-Sam@users.noreply.github.com>
2023-05-13 23:20:08 +0200

    lesson_7

---
+++ lesson_7_.py
@@ -0,0 +1,7 @@
+# Defining the given phrase
+phrase = "ReDI school is awesome! Yes ReDI is really cool"
+
+# Counting occurrences of 'ReDI'
+occurrences: int = phrase.count("ReDI")
+
+print(f"The word 'ReDI' occurs {occurrences} times in the phrase.")
